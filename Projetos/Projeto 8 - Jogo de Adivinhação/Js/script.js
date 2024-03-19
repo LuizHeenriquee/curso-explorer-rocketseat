5d28dc13a0532208801fe53ea613c0668db2e52e
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-03-19 18:22:01 -0300

    I redid the activity to fix the content and ended up doing some things differently, even though everything is working.

--- Projetos/Projeto 8 - Jogo de Adivinhação/Js/script.js
+++ Projetos/Projeto 8 - Jogo de Adivinhação/Js/script.js
@@ -1,31 +1,35 @@
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
+const winnerMessage = document.querySelector('.screen2 h2')
 const randomNumber = Math.round(Math.random() * 10)
 let attempts = 1
 
 const handleTryClick = (event) => {
-    event.preventDefault() // Essa linha está aqui para evitar que seja feito o padrão ao clicar no button. Isso se deve (neste caso) pois o button está dentro de um form, logo ele "envia e recarrega" a página. Com essa linha, essas funções são evitadas.
+    event.preventDefault()
 
     const inputNumber = document.querySelector('#inputNumber')
 
-    if (Number(inputNumber.value) == randomNumber) {
+    if (Number(inputNumber.value) == randomNumber && attempts == 1) {
         screen1.classList.add('hide')
         screen2.classList.remove('hide')
-        document.querySelector('.screen2 h2').innerText = `Você acertou em ${attempts} tentativas!`
+        winnerMessage.innerText = `Parabéns! Você acertou de primeira!`
+    } else if (Number(inputNumber.value) == randomNumber) {
+        screen1.classList.add('hide')
+        screen2.classList.remove('hide')
+        winnerMessage.innerText = `Parabéns! Você acertou em ${attempts} tentativas!`
     }
 
     inputNumber.value = ''
     attempts++
 }
 
-// Eventos
 const btnTry = document.querySelector('#btnTry')
 const btnReset = document.querySelector('#btnReset')
 
-// Função CallBack
 btnTry.addEventListener('click', handleTryClick)
-btnReset.addEventListener('click', function () {
+btnReset.addEventListener('click', function() {
     screen1.classList.remove('hide')
     screen2.classList.add('hide')
     attempts = 1
+    location.reload()
 })
