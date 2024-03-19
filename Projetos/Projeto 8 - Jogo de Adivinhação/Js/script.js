ed11e5b494a5f2a4403d073e3cff159f9c98ed28
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-03-19 17:42:48 -0300

    Made the guessing game using HTML and Js.

--- Projetos/Projeto 8 - Jogo de Adivinhação/Js/script.js
+++ Projetos/Projeto 8 - Jogo de Adivinhação/Js/script.js
@@ -1,16 +1,31 @@
+const screen1 = document.querySelector('.screen1')
+const screen2 = document.querySelector('.screen2')
 const randomNumber = Math.round(Math.random() * 10)
 let attempts = 1
 
-const handleClick = (event) => {
-    event.preventDefault()
+const handleTryClick = (event) => {
+    event.preventDefault() // Essa linha está aqui para evitar que seja feito o padrão ao clicar no button. Isso se deve (neste caso) pois o button está dentro de um form, logo ele "envia e recarrega" a página. Com essa linha, essas funções são evitadas.
 
     const inputNumber = document.querySelector('#inputNumber')
 
     if (Number(inputNumber.value) == randomNumber) {
-        document.querySelector('.screen1').classList.add('hide')
-        document.querySelector('.screen2').classList.remove('hide')
+        screen1.classList.add('hide')
+        screen2.classList.remove('hide')
         document.querySelector('.screen2 h2').innerText = `Você acertou em ${attempts} tentativas!`
     }
 
+    inputNumber.value = ''
     attempts++
 }
+
+// Eventos
+const btnTry = document.querySelector('#btnTry')
+const btnReset = document.querySelector('#btnReset')
+
+// Função CallBack
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', function () {
+    screen1.classList.remove('hide')
+    screen2.classList.add('hide')
+    attempts = 1
+})
