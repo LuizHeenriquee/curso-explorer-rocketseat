387288eea33b86597548dc369b3eb3eb665ded79
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-05 16:07:36 -0300

    I deleted unnecessary files for the project, renamed and organized the new files into folders and the project can now be started.

---
+++ React/rocketnotes/src/Details/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+export function Details() {
+    return (
+        <>
+            <h1>Hello, world!</h1>
+            <p>Meu nome é Luiz Henrique</p>
+        </>
+    )
+}
