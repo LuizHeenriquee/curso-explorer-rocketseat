b2f1d5bdc20ce528c6bb7d6295c815fdc523c783
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-08 16:42:19 -0300

    The Profile component was created to store user information within the Header. There we pass an image and a div that contains a welcome message and then the user's name. Within styles.js, this entire code component was customized, including applying some specificity ideas.

--- React/rocketnotes/src/components/Header/index.jsx
+++ React/rocketnotes/src/components/Header/index.jsx
@@ -1,9 +1,18 @@
-import { Container } from "./styles"
+import { Container, Profile } from "./styles"
 
 export function Header() {
     return(
         <Container>
-            
+            <Profile>
+                <img 
+                    src="https://github.com/LuizHeenriquee.png"
+                    alt="Foto do Usuário"
+                />
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Luiz Henrique</strong>
+                </div>
+            </Profile>
         </Container>
     )
 }
