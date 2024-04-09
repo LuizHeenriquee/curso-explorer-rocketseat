be4ed2d09a578d0aca4c0d7aedcf58c0760159ca
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-09 16:51:39 -0300

    React Icons library installation

--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
-import {Details} from "./Details"
+import { Details } from "./Details"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-        <Details/>
+      <Details/>
     </ThemeProvider>
   </React.StrictMode>,
 )
