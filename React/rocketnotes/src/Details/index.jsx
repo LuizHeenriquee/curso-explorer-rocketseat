95f8e077b5b57dd98a15111b6e0993c6e0cc01e5
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-09 18:27:20 -0300

    Tag component creation.

--- React/rocketnotes/src/Details/index.jsx
+++ React/rocketnotes/src/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from "./styles"
 import { Header } from "../components/Header"
 import { Button } from "../components/Button"
 import { Section } from "../components/Section"
+import { Tag } from "../components/Tag"
 
 export function Details() {
     return (
@@ -12,6 +13,10 @@ export function Details() {
                     <li><a href="#">https://www.rocketseat.com.br</a></li>
                     <li><a href="#">https://www.rocketseat.com.br</a></li>
                 </Links>
+            </Section>
+            <Section title="Marcadores">
+                <Tag title="express"/>
+                <Tag title="nodejs"/>
             </Section>
             <Button title="Voltar"/>
         </Container>
