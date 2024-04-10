800260f933af7dfb77604c1590888bce45268197
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-10 10:11:22 -0300

    I inserted the main tag so that it could receive grid-area: content and then apply the scroll only to that space, leaving the header fixed. Furthermore, the Content component was created to style the content and its internal tags.

--- React/rocketnotes/src/Details/index.jsx
+++ React/rocketnotes/src/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles"
+import { Container, Links, Content } from "./styles"
 import { Header } from "../components/Header"
 import { Button } from "../components/Button"
 import { Section } from "../components/Section"
@@ -9,18 +9,26 @@ export function Details() {
     return (
         <Container>
             <Header/>
-            <ButtonText title="Excluir nota"/>
-            <Section title="Links úteis">
-                <Links>
-                    <li><a href="#">https://www.rocketseat.com.br</a></li>
-                    <li><a href="#">https://www.rocketseat.com.br</a></li>
-                </Links>
-            </Section>
-            <Section title="Marcadores">
-                <Tag title="express"/>
-                <Tag title="nodejs"/>
-            </Section>
-            <Button title="Voltar"/>
+            <main>
+                <Content>
+                    <ButtonText title="Excluir nota"/>
+                    <h1>Introdução ao React</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod animi beatae tenetur illum molestias obcaecati dolorem nisi expedita. Doloribus dolore iure velit similique magni voluptatibus modi eum molestias fugit atque!
+                    </p>
+                    <Section title="Links úteis">
+                        <Links>
+                            <li><a href="#">https://www.rocketseat.com.br</a></li>
+                            <li><a href="#">https://www.rocketseat.com.br</a></li>
+                        </Links>
+                    </Section>
+                    <Section title="Marcadores">
+                        <Tag title="express"/>
+                        <Tag title="nodejs"/>
+                    </Section>
+                    <Button title="Voltar"/>
+                </Content>
+            </main>
         </Container>
     )
 }
