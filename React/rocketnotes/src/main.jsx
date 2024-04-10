8450cfd99c302d54bf1881e845fbe1dd24cc1bd7
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-10 17:05:00 -0300

    I started developing the project's Home page. I created a new folder and added the jsx and js files inside. I used display grid to better distribute content on the page.

--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
-import { Details } from "./Details"
+// import { Details } from "./Details"
+import { Home } from './Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Details/>
+      <Home/>
     </ThemeProvider>
   </React.StrictMode>,
 )
