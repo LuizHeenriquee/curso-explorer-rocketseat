c5af3b9f69b69ae60568d5dd70556c9c060f30c6
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-11 18:33:26 -0300

    I started developing the Sign In page.

---
+++ React/rocketnotes/src/SingIn/index.jsx
@@ -0,0 +1,32 @@
+import { Input } from "../components/Input"
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { Button } from "../components/Button"
+import { Container, Form } from "./styles"
+
+export function SignIn() {
+    return(
+        <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>
+                    Aplicação para salvar e gerenciar seus links úteis.
+                </p>
+                <h2>Faça seu login</h2>
+                <Input
+                    placeholder="Email"
+                    type="text"
+                    icon={FiMail}
+                />
+                <Input
+                    placeholder="senha"
+                    type="password"
+                    icon={FiLock}
+                />
+                <Button title="Entrar"/>
+                <a href="#">
+                    Criar Conta
+                </a>
+            </Form>
+        </Container>
+    )
+}
--- React/rocketnotes/src/main.jsx
+++ React/rocketnotes/src/main.jsx
@@ -4,13 +4,14 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyle from './styles/global'
 // import { Details } from "./Details"
-import { Home } from './Home'
+// import { Home } from './Home'
+import { SignIn } from './SingIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Home/>
+      <SignIn/>
     </ThemeProvider>
   </React.StrictMode>,
 )
