b1725b5613f095d07a68cf1a91bfd7cc0ee4f127
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-11 16:34:07 -0300

    Development of the note component using the data property.

--- React/rocketnotes/src/Home/index.jsx
+++ React/rocketnotes/src/Home/index.jsx
@@ -1,7 +1,9 @@
 import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../components/Header'
+import { Note } from '../components/Note'
 import { Input } from '../components/Input'
+import { Section } from '../components/Section'
 import { ButtonText } from '../components/ButtonText'
 
 export function Home() {
@@ -26,7 +28,26 @@ export function Home() {
                 <Input placeholder="Pesquisar pelo título"/>
             </Search>
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note
+                        data={{
+                            title: 'React Modal',
+                            tags: [
+                                {id: '1', name: 'react'},
+                                {id: '2', name: 'rokcetseat'}
+                            ]
+                        }}
+                    />
+                    <Note
+                        data={{
+                            title: 'Exemplo de Middleware',
+                            tags: [
+                                {id: '1', name: 'express'},
+                                {id: '2', name: 'node.js'}
+                            ]
+                        }}
+                    />
+                </Section>
             </Content>
             <NewNote>
                 <FiPlus/>
