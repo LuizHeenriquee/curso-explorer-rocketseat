9a7a2197a83182496266876a3da3a65f9fb2d593
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-04-11 14:33:01 -0300

    Developed the Input component.

--- React/rocketnotes/src/Home/index.jsx
+++ React/rocketnotes/src/Home/index.jsx
@@ -1,6 +1,7 @@
 import { FiPlus } from 'react-icons/fi'
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 import { Header } from '../components/Header'
+import { Input } from '../components/Input'
 import { ButtonText } from '../components/ButtonText'
 
 export function Home() {
@@ -22,7 +23,7 @@ export function Home() {
                 </li>
             </Menu>
             <Search>
-
+                <Input placeholder="Pesquisar pelo título"/>
             </Search>
             <Content>
 
